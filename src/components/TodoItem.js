7307687306dc8ca2891b169d571fc0f36c05d0cd
Nicholas Mutale <nicomutale@gmail.com>
2022-04-05 23:57:31 +0200

    add style when completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,17 +3,28 @@ import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
-    return <li className={styles.item}>
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    return (
+      <li className={styles.item}>
         <input
-         type="checkbox"
-         className={styles.checkbox}
-         checked={this.props.todo.completed}
-         onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
-         <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-            Delete
-         </button>
-         {this.props.todo.title}
-        </li>
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
+      </li>
+    )
   }
 }
 
