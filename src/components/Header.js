7ed7a36918e0bd183dc02c08f98b32ce1d8152fa
Nicholas Mutale <nicomutale@gmail.com>
2022-04-05 23:21:55 +0200

    add inline style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react"
 
 const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
   return (
-    <header>
-      <h1>todos</h1>
+    <header style={headerStyle}>
+      <h1
+      style={{
+        fontsize: "6rem",
+        fontweight: "600",
+        marginBottom: "2rem",
+        lineHeight: "1em",
+        color: "red",
+        textTransform: "lowercase",
+        textAlign: "center",
+      }}
+      >todos</h1>
     </header>
   )
 }
