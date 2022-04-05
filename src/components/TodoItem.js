dbad7861f391cbd0423f0795d3fa50bef9a51dcc
Nicholas Mutale <nicomutale@gmail.com>
2022-04-05 23:36:57 +0200

    add style use css module

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,12 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
-    return <li>
+    return <li className={styles.item}>
         <input
          type="checkbox"
+         className={styles.checkbox}
          checked={this.props.todo.completed}
          onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
